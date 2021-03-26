a023860069d0ec1058b4ed141a9da791654a4f9c
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-03-26 11:45:22 +0300

    Create CreateNew simple test

---
+++ src/Repetitorg/CoreTest/ClientTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repetitorg.Core;
+
+namespace CoreTest
+{
+    [TestFixture]
+    class ClientTest
+    {
+        [TestCase]
+        public void CreateNew_CreateSimpleClient_IncrementClientsCount()
+        {
+            Client c = Client.CreateNew("Иванов Иван Иванович");
+            Assert.AreEqual(1, Client.ClientsCount);
+        }
+    }
+}
