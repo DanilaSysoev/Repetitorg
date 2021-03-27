307d65a43e5c7d711394571529ff33632d2401dd
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-03-27 12:13:10 +0300

    Update PhoneNumber set property

--- src/Repetitorg/Core/Client.cs
+++ src/Repetitorg/Core/Client.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repetitorg.Core.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace Repetitorg.Core
             }
             set
             {
+                if (value == null)
+                    throw new InvalidPhoneNumberException("PhoneNumber can't be null");
                 phoneNumber = value;
             }
         }
