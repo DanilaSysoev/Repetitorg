5136c91bae3fcad9121d7401dffe5e71bc789f84
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-05-31 12:10:19 +0300

    Add AddOnDate_AddFourTaskOnDifferentDate_AllTasksExist test

--- src/Repetitorg/CoreTest/TaskTests.cs
+++ src/Repetitorg/CoreTest/TaskTests.cs
@@ -71,5 +71,27 @@ namespace CoreTest
             Assert.IsTrue(tasks.Contains(task2));
             Assert.IsTrue(tasks.Contains(task3));
         }
+        [TestCase]
+        public void AddOnDate_AddFourTaskOnDifferentDate_AllTasksExist()
+        {
+            Task task1 = Task.AddOnDate("2020/12/30 test task 1", new DateTime(2020, 12, 30));
+            Task task2 = Task.AddOnDate("2020/12/30 test task 2", new DateTime(2019, 11, 20));
+            Task task3 = Task.AddOnDate("2020/12/30 test task 3", new DateTime(2018, 10, 10));
+            Task task4 = Task.AddOnDate("2020/12/30 test task 3", new DateTime(2017, 10, 5));
+            List<Task> tasks1 = Task.GetByDate(new DateTime(2020, 12, 30));
+            List<Task> tasks2 = Task.GetByDate(new DateTime(2019, 11, 20));
+            List<Task> tasks3 = Task.GetByDate(new DateTime(2018, 10, 10));
+            List<Task> tasks4 = Task.GetByDate(new DateTime(2017, 10, 5));
+
+            Assert.AreEqual(4, Task.TasksCount);
+            Assert.AreEqual(1, tasks1);
+            Assert.AreEqual(1, tasks2);
+            Assert.AreEqual(1, tasks3);
+            Assert.AreEqual(1, tasks4);
+            Assert.IsTrue(tasks1.Contains(task1));
+            Assert.IsTrue(tasks2.Contains(task2));
+            Assert.IsTrue(tasks3.Contains(task3));
+            Assert.IsTrue(tasks4.Contains(task4));
+        }
     }
 }
