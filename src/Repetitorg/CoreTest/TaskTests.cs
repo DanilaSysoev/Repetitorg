4c69d42ec5d3507d5873e31bd56742ca1e462816
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-05-31 11:53:32 +0300

    Add AddTomorrow_AddThreeTasks_AllTasksExist test

--- src/Repetitorg/CoreTest/TaskTests.cs
+++ src/Repetitorg/CoreTest/TaskTests.cs
@@ -30,5 +30,18 @@ namespace CoreTest
             List<Task> tomorrowTasks = Task.GetTomorrowTasks();
             Assert.AreEqual(task, tomorrowTasks[0]);
         }
+        [TestCase]
+        public void AddTomorrow_AddThreeTasks_AllTasksExist()
+        {
+            Task task1 = Task.AddTomorrowTask("Tomorrow test task 1");
+            Task task2 = Task.AddTomorrowTask("Tomorrow test task 2");
+            Task task3 = Task.AddTomorrowTask("Tomorrow test task 3");
+            List<Task> tomorrowTasks = Task.GetTomorrowTasks();
+
+            Assert.AreEqual(3, tomorrowTasks.Count);
+            Assert.IsTrue(tomorrowTasks.Contains(task1));
+            Assert.IsTrue(tomorrowTasks.Contains(task1));
+            Assert.IsTrue(tomorrowTasks.Contains(task1));
+        }
     }
 }
