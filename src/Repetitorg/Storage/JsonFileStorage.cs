1204bb0993cb1da9f37622f001ff79e79a6e506e
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-05-31 19:52:16 +0300

    Add JsonFileStorage generic

---
+++ src/Repetitorg/Storage/JsonFileStorage.cs
@@ -0,0 +1,20 @@
+﻿using Repetitorg.Core.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repetitorg.Storage
+{
+    public class JsonFileStorage<T> : IStorage<T>
+    {
+        public void Save(List<T> projects)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
