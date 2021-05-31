a71fca0d8c9c1c7a2edb68107ce20ed26623b407
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-05-31 11:40:25 +0300

    Impl AddTomorrowTask

--- src/Repetitorg/Core/Task.cs
+++ src/Repetitorg/Core/Task.cs
@@ -6,14 +6,53 @@ namespace Core
 {
     public class Task
     {
-        public static int TasksCount { get; set; }
+        public static int TasksCount
+        {
+            get
+            {
+                return tasks.Count;
+            }
+        }
 
         public static void Clear()
-        { }
-
-        public static void AddTomorrowTask(string v)
         {
-            throw new NotImplementedException();
+            tasks.Clear();
+        }
+
+        public static void AddTomorrowTask(string taskName)
+        {
+            tasks.Add(new Task(taskName, DateTime.Now.Date.AddDays(1)));
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return taskName; 
+            }
+        }
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+        }
+
+        private static List<Task> tasks;
+        private string taskName;
+        private DateTime date;
+
+        private Task(string taskName, DateTime date)
+        {
+            this.taskName = taskName;
+            this.date = date;
+        }
+
+        static Task()
+        {
+            tasks = new List<Task>();
         }
     }
 }
