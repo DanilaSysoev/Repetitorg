bf26635a141cd9e18c37d2d44d17fe0588762f79
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-05-31 16:13:12 +0300

    add ITaskStorage interface

---
+++ src/Repetitorg/Core/Base/ITaskStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Base
+{
+    public interface ITaskStorage
+    {
+        void Save();
+        void Load();
+    }
+}
