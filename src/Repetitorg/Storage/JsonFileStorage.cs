182c137cd33bfae956b1eaa3cf5edb8f883eed62
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-05-31 19:55:54 +0300

    Implement JsonFileStorage<T> generic

--- src/Repetitorg/Storage/JsonFileStorage.cs
+++ src/Repetitorg/Storage/JsonFileStorage.cs
@@ -1,20 +1,42 @@
-﻿using Repetitorg.Core.Base;
+﻿using Newtonsoft.Json;
+using Repetitorg.Core.Base;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Repetitorg.Storage
 {
     public class JsonFileStorage<T> : IStorage<T>
     {
-        public void Save(List<T> projects)
+        private string path;
+
+        public JsonFileStorage(string path)
         {
-            throw new NotImplementedException();
+            this.path = path;
+        }
+
+        public void Save(List<T> tasks)
+        {
+            var dataPath = Path.Combine(path + DATA_PATH);
+            if (!Directory.Exists(Path.GetDirectoryName(dataPath)))
+                Directory.CreateDirectory(Path.GetDirectoryName(dataPath));
+
+            var data = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+
+            using (StreamWriter writer = new StreamWriter(dataPath))
+                writer.Write(data);
         }
 
         public List<T> Load()
         {
-            throw new NotImplementedException();
+            var data = "";
+            var dataPath = Path.Combine(path + DATA_PATH);
+            using (StreamReader reader = new StreamReader(dataPath))
+                data = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<List<T>>(data);
         }
+
+        private static string DATA_PATH = "/data/" + typeof(T).Name.ToLower() + "_data.json";
     }
 }
