56fb57a5c7cb39042481a6d2c2bb7df32ba8f135
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-05-31 17:08:28 +0300

    fix compilation errors

---
+++ src/Repetitorg/Core/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repetitorg.Core
+{
+    public class Project
+    {
+        public static int ProjectsCount
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public static void Add(string name)
+        { }
+        public static void Clear()
+        { }
+    }
+}
--- src/Repetitorg/CoreTest/ProjectTests.cs
+++ src/Repetitorg/CoreTest/ProjectTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using Repetitorg.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
