4e46953ae234f5fdcb6aecbcd5d86836d28a21c7
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-05-31 20:02:10 +0300

    Add SaveLoad_CompleteAndSaveLoad_CompleteStatusSaved test

--- src/Repetitorg/StorageTest/JsonFileProjectsStorageTests.cs
+++ src/Repetitorg/StorageTest/JsonFileProjectsStorageTests.cs
@@ -37,6 +37,24 @@ namespace Repetitorg.StorageTest
             Assert.IsTrue(Project.GetAll().Contains(p2));
             Assert.IsTrue(Project.GetAll().Contains(p3));
         }
+        [TestCase]
+        public void SaveLoad_CompleteAndSaveLoad_CompleteStatusSaved()
+        {
+            Project p1 = Project.Add("Test project 1");
+            Project p2 = Project.Add("Test project 2");
+            Project p3 = Project.Add("Test project 3");
+
+            Project.Complete(p1);
+            var storage = new JsonFileStorage<Project>(TEST_DATA_PATH);
+            Project.Save(storage);
+            Project.Clear();
+            Project.Load(storage);
+
+            Assert.IsTrue(Project.FindByName("Test project 1")[0].Completed);
+
+            Assert.IsFalse(Project.FindByName("Test project 2")[0].Completed);
+            Assert.IsFalse(Project.FindByName("Test project 3")[0].Completed);
+        }
 
         private const string TEST_DATA_PATH = "D:\\YandexDisk\\YandexDisk\\Danila\\Work\\Repetitorg";
     }
