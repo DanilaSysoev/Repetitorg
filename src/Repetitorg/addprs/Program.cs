1bdc76f39b6495581133035b44cfa28199178fe6
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-06-01 18:00:43 +0300

    implement addprjs

--- src/Repetitorg/addprs/Program.cs
+++ src/Repetitorg/addprs/Program.cs
@@ -1,4 +1,6 @@
-﻿using Repetitorg.UtilitesSharedService;
+﻿using Repetitorg.Core;
+using Repetitorg.Storage;
+using Repetitorg.UtilitesSharedService;
 using System;
 
 namespace Repetitorg.addprs
@@ -20,6 +22,15 @@ namespace Repetitorg.addprs
 
         private static void MainProcess(string[] args)
         {
+            JsonFileStorage<Project> storage =                
+                new JsonFileStorage<Project>(ConsoleServices.Config.GetField(DATA_PATH));
+            Project.Load(storage);
+            foreach (var project in args)
+            {
+                Project.Add(project);
+                Project.Save(storage);
+                Console.WriteLine("{0} added!", project);
+            }
         }
 
         private const string DATA_PATH = "data_path";
