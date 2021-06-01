c79d57866ea9b2b345b842e11e757afcf7a6ffe2
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-06-01 12:51:53 +0300

    add SaveLoad_AttachToTaskAndSaveLoad_AttachSaved test

--- src/Repetitorg/StorageTest/JsonFileTasksStorageTests.cs
+++ src/Repetitorg/StorageTest/JsonFileTasksStorageTests.cs
@@ -55,6 +55,27 @@ namespace Repetitorg.StorageTest
             Assert.IsFalse(Task.GetByDate(new DateTime(2020, 12, 30))[0].Completed);
             Assert.IsFalse(Task.GetByDate(new DateTime(2020, 10, 20))[0].Completed);
         }
+        [TestCase]
+        public void SaveLoad_AttachToTaskAndSaveLoad_AttachSaved()
+        {
+            Task task1 = Task.AddOnDate("2020/12/30 test task 1", new DateTime(2020, 12, 30));
+            Task task2 = Task.AddOnDate("2020/11/10 test task 2", new DateTime(2020, 11, 10));
+            Task task3 = Task.AddOnDate("2020/10/20 test task 3", new DateTime(2020, 10, 20));
+
+            Project p1 = Project.Add("Test project 1");
+            Project p2 = Project.Add("Test project 2");
+
+            Task.AttachToProject(task1, p1);
+            Task.AttachToProject(task2, p2);
+            var storage = new JsonFileStorage<Task>(TEST_DATA_PATH);
+            Task.Save(storage);
+            Task.Clear();
+            Task.Load(storage);
+
+            Assert.AreEqual(p1, Task.GetByDate(new DateTime(2020, 12, 30))[0].Project);
+            Assert.AreEqual(p2, Task.GetByDate(new DateTime(2020, 11, 10))[0].Project);
+            Assert.IsNull(Task.GetByDate(new DateTime(2020, 10, 20))[0].Project);
+        }
 
         private const string TEST_DATA_PATH = "D:\\YandexDisk\\YandexDisk\\Danila\\Work\\Repetitorg";
     }
