62172054523be43b9a4bb69d62138dc29e1b1109
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-06-01 20:23:17 +0300

    add empty prs implementation

---
+++ src/Repetitorg/prs/Program.cs
@@ -0,0 +1,25 @@
+﻿using Repetitorg.UtilitiesSharedService;
+using System;
+
+namespace prs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ConsoleServices.ReadConfig();
+                MainProcess(args);
+            }
+            catch (Exception e)
+            {
+                ConsoleServices.ExceptionHandle("prs", e);
+            }
+        }
+
+        private static void MainProcess(string[] args)
+        {
+        }
+    }
+}
