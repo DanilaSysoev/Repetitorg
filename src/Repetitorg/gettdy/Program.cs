ab51f848f866de33f416b5ff224505d8c06d6bb1
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-06-02 13:45:03 +0300

    implement gettdy

--- src/Repetitorg/gettdy/Program.cs
+++ src/Repetitorg/gettdy/Program.cs
@@ -1,5 +1,8 @@
-﻿using Repetitorg.UtilitiesSharedService;
+﻿using Repetitorg.Core;
+using Repetitorg.Storage;
+using Repetitorg.UtilitiesSharedService;
 using System;
+using System.Collections.Generic;
 
 namespace Repetitorg.gettdy
 {
@@ -20,6 +23,33 @@ namespace Repetitorg.gettdy
 
         private static void MainProcess(string[] args)
         {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Please, use\ngettdy\nformat");
+                return;
+            }
+
+            JsonFileStorage<Task> storage =
+                new JsonFileStorage<Task>(
+                    ConsoleServices.Config.GetField(ConsoleServices.DATA_PATH)
+                );
+            Task.Load(storage);
+
+            List<Task> tasks = Task.GetByDate(DateTime.Now.Date);
+
+            PrintTasks(tasks);
+        }
+
+        private static void PrintTasks(List<Task> tasks)
+        {
+            int num = 1;
+            foreach (var task in tasks)
+            {
+                if (task.Completed)
+                    Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0, 2}. {1}", num++, task.Name);
+                Console.ResetColor();
+            }
         }
     }
 }
