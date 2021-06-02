33c8a3fb0753d413ecb57e0bdddcdb1630fccfe1
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-06-02 13:39:57 +0300

    update references

--- src/Repetitorg/gettdy/Program.cs
+++ src/Repetitorg/gettdy/Program.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Repetitorg.UtilitiesSharedService;
+using System;
 
 namespace Repetitorg.gettdy
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            try
+            {
+                ConsoleServices.ReadConfig();
+                MainProcess(args);
+            }
+            catch (Exception e)
+            {
+                ConsoleServices.ExceptionHandle("gettdy", e);
+            }
+        }
+
+        private static void MainProcess(string[] args)
         {
         }
     }
