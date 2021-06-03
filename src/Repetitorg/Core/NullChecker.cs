342df79c30534c4f9f6f1c9771dad6d023a517a1
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-06-03 14:28:12 +0300

    add NullChecker

---
+++ src/Repetitorg/Core/NullChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repetitorg.Core
+{
+    class NullChecker
+    {
+        private string message = "";
+
+        public void Add(object argument, string message)
+        {
+            if (argument == null)
+                this.message += message + "\n";
+        }
+        public static void Check(object argument, string message)
+        {
+            if (message != "")
+                throw new ArgumentException(message);
+        }
+    }
+}
