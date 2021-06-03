bf439ac02ed6ec4bbb0292f7bb38e26185256cbe
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-06-03 14:54:45 +0300

    add GetWithotProject_GettingExistingWithoutProject_ReturnAllWithotProject test

--- src/Repetitorg/CoreTest/TaskTests.cs
+++ src/Repetitorg/CoreTest/TaskTests.cs
@@ -399,6 +399,23 @@ namespace Repetitorg.CoreTest
             Assert.IsTrue(tasks.Contains(task3));
         }
 
+        [TestCase]
+        public void GetWithotProject_GettingExistingWithoutProject_ReturnAllWithotProject()
+        {
+            Task task1 = Task.AddOnDate("2020/12/30 test task 1", new DateTime(2020, 12, 30));
+            Task task2 = Task.AddOnDate("2020/11/30 test task 2", new DateTime(2020, 11, 30));
+            Task task3 = Task.AddOnDate("2020/10/20 test task 3", new DateTime(2020, 10, 20));
+
+            Project p1 = Project.Add("Test project 1");
+            Project p2 = Project.Add("Test project 2");
+
+            Task.AttachToProject(task1, p2);
+
+            List<Task> tasks = Task.GetWithoutProject();
+            Assert.AreEqual(2, tasks.Count);
+            Assert.IsTrue(tasks.Contains(task2));
+            Assert.IsTrue(tasks.Contains(task3));
+        }
         private const string TEST_DATA_PATH = "D:\\YandexDisk\\YandexDisk\\Danila\\Work\\Repetitorg";
     }
 }
