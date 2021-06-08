a4111a3ac6c55836fd34dca38defe941eee5f27a
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-06-08 19:08:02 +0300

    implement Equals, ToString, GetHashCode

--- src/Repetitorg/Core/Person.cs
+++ src/Repetitorg/Core/Person.cs
@@ -30,5 +30,27 @@ namespace Repetitorg.Core
             this.fullName = fullName;
             this.phoneNumber = phoneNumber;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person)
+            {
+                Person client = (Person)obj;
+                return client.PhoneNumber == PhoneNumber && 
+                       client.FullName == FullName && 
+                       GetType().Equals(obj.GetType());
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (fullName.GetHashCode() + phoneNumber.GetHashCode()) * 31;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", fullName, phoneNumber);
+        }
     }
 }
