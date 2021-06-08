6e6062813934168f37862b72cea3ded117d557c1
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-06-08 17:48:35 +0300

    create SelfFactory interface

---
+++ src/Repetitorg/Core/Base/SelfFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repetitorg.Core.Base
+{
+    public interface SelfFactory<T>
+    {
+        T CreateInstance(params object[] argumenst);
+    }
+}
---
+++ src/Repetitorg/Core/EntityCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repetitorg.Core
+{
+    class EntityCollection<T>
+    {
+        private static List<T> entities;
+
+        public static int Count
+        {
+            get
+            {
+                return entities.Count;
+            }
+        }
+        public static Client CreateNew(string fullName, string phoneNumber = "")
+        {
+            new NullChecker().
+                Add(fullName, "Can not create client with NULL name").
+                Add(phoneNumber, "Can not create client with NULL phone number").
+                Check();
+
+            var entity = new T(fullName, phoneNumber);
+
+            if (entities.Contains(client))
+                throw new InvalidOperationException(
+                    "Creation clients with same names and phone numbers is impossible"
+                );
+
+            entities.Add(client);
+            return client;
+        }
+        public static List<Client> GetAll()
+        {
+            return new List<Client>(entities);
+        }
+        public static IList<Client> FilterByName(string condition)
+        {
+            return
+                (from client in entities
+                 where client.fullName.ToLower().Contains(condition.ToLower())
+                 select client).ToList();
+        }
+        public static void Clear()
+        {
+            entities.Clear();
+        }
+        static EntityCollection()
+        {
+            entities = new List<T>();
+        }
+    }
+}
