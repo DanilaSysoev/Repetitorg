acc3262dc6dbd7690a9b62c0bd1db19b9dcd2b6d
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-08-30 16:00:09 +0300

    add order.clear (fce)

---
+++ src/Repetitorg/Core/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repetitorg.Core
+{
+    public class Order
+    {
+        public static void Clear()
+        { }
+    }
+}
