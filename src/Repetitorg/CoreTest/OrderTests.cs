a160bb3549322de843ae243a8990e083bff5d23f
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-08-30 16:07:15 +0300

    add setup method

--- src/Repetitorg/CoreTest/OrderTests.cs
+++ src/Repetitorg/CoreTest/OrderTests.cs
@@ -18,5 +18,17 @@ namespace Repetitorg.CoreTest
             Client.Clear();
             Student.Clear();
         }
+        [SetUp]
+        public void Initialize()
+        {
+            Client.CreateNew("c1");
+            Client.CreateNew("c2");
+            Client.CreateNew("c3");
+
+            Student.CreateNew("s1");
+            Student.CreateNew("s2");
+            Student.CreateNew("s3");
+        }
+
     }
 }
