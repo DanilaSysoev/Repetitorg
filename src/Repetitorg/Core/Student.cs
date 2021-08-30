4a8e366dd72fb22b1d3883f8c00ad5b9c469ea0e
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-08-30 17:47:55 +0300

    simple implement Student.AttachToClient

--- src/Repetitorg/Core/Student.cs
+++ src/Repetitorg/Core/Student.cs
@@ -6,9 +6,24 @@ namespace Repetitorg.Core
 {
     public class Student : PersonsCollection<Student>
     {
+        public Client Client
+        {
+            get
+            {
+                return client;
+            }
+        }
+
+        public void AttachToClient(Client client)
+        {
+            this.client = client;
+        }
+
         internal Student(string fullName, string phoneNumber)
             : base(fullName, phoneNumber)
         { }
+
+        private Client client;
     }
 
 }
