de2a25c427fe61c77bc9b796283b22defa20ce23
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2022-04-01 12:51:33 +0300

    add Create_CreateWithCorrectArgs_LessonCountIncrease and setup block

--- src/Repetitorg/CoreTest/LessonTests.cs
+++ src/Repetitorg/CoreTest/LessonTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using Repetitorg.Core;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,24 @@ namespace Repetitorg.CoreTest
     [TestFixture]
     class LessonTests
     {
+        private DummyOrderStorage orders;
+        private DummyLessonStorage lessons;
+        [SetUp]
+        public void Setup()
+        {
+            orders = new DummyOrderStorage();
+            lessons = new DummyLessonStorage();
+            Order.InitializeStorage(orders);
+            Lesson.InitializeStorage(lessons);
+        }
+
+        [TestCase]
+        public void Create_CreateWithCorrectArgs_LessonCountIncrease()
+        {
+            Order order = Order.CreateNew("test order");
+            Assert.AreEqual(0, Lesson.Count);
+            Lesson lesson = Lesson.Create(new DateTime(2021, 10, 10, 12, 0, 0), 90, order);
+            Assert.AreEqual(1, Lesson.Count);
+        }
     }
 }
